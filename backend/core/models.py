760b22321f738b383c776d80315e22d066763f9f
denistendo <ssebyaladenis18@gmail.com>
2026-06-15 21:23:44 +0300

    added InternetPackage model to core app

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,1 +1,7 @@
 from django.db import models
+
+class InternetPackage(models.model):
+    name = models.Charfield(max_length=100, unique=True, help_text="Enter the name of the package")
+    price = models.DecimalField(max_digits=10, decimal_places=2, help_text="Enter the price of the package")
+    duration_hours
+    
